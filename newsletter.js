a1edb4cb9b6e89d71a888594415030bde483af98
asormar <sorolladev@gmail.com>
2026-01-17 16:29:32 +0100

    Transporter created

---
+++ newsletter.js
@@ -0,0 +1,14 @@
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // true para port 465
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
+});
